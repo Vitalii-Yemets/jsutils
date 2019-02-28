f0f3d9d31c705cabe3b8cd845efd624cbe93c3d9
Vitalii Yemets <vitalii.yemets@outlook.com>
2019-02-28 04:00:52 +0200

    Fixed import instruction for indexOf operator

--- src/arrays/mergeUnique.js
+++ src/arrays/mergeUnique.js
@@ -1,4 +1,4 @@
-import indexOf from './src/arrays/indexOf'
+import indexOf from './indexOf'
 
 const mergeUnique = (arr1, arr2, filterCallback) => arr1.concat(arr2.filter(el2 => indexOf(arr1, el1 => filterCallback(el1, el2)) === -1))
 
