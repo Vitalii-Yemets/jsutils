bdd8f645e3d1b223265a057e4be4c55cbf0d6085
Vitalii Yemets <vitalii.yemets@outlook.com>
2019-03-11 00:05:49 +0200

    Added declare instructions

--- convertors/index.d.ts
+++ convertors/index.d.ts
@@ -1,4 +1,4 @@
-export function fromUtf8(str: string): string
-export function toBytes(str: string): Array<number>
-export function toDate(date: string): Date | null
-export function toUtf8(str: string): Array<number | any>
+export declare function fromUtf8(str: string): string
+export declare function toBytes(str: string): Array<number>
+export declare function toDate(date: string): Date | null
+export declare function toUtf8(str: string): Array<number | any>
