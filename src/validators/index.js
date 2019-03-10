c4b744f46ef636e77d82eb828248028a754eb3cf
Vitalii Yemets <vitalii.yemets@outlook.com>
2019-03-10 18:11:12 +0200

    Changed export instructions

---
+++ src/validators/index.js
@@ -0,0 +1,13 @@
+import isEmpty from './isEmpty'
+import isJson from './isJson'
+import isLength from './isLength'
+import isWalletAddress from './isWalletAddress'
+import StringValidator from './string'
+
+export {
+    isEmpty,
+    isJson,
+    isLength,
+    isWalletAddress,
+    StringValidator
+}
